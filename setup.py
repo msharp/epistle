014d69b5c5f96f28d761c884ae5c1e31dad188a6
Max Sharples <maxsharples@gmail.com>
2014-02-18 19:54:11 +1100

    more placeholder notes in setup script

--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
 
 # setup epistle for version-controlled not taking
 
+# create dir: ~/.epistle
+# create file: ~/.epistle/epistle.ini
+
+# get input => git repo (gist)
+# transform: https://gist.github.com/2fcae0fc9df815f82567.git
+# to: git@github.com:2fcae0fc9df815f82567.git 
+
+# checkout the repo to: ~/.epistle/epistles
+#
 # symlink the script
 
 # chmod the script
 
 # create the config file
+"""
+[Git]
+Repo: %s
+""" % repo
 
 
