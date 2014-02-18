8ce1477c45b28193826f1adfd23f6d0a3e98cfc2
Max Sharples <maxsharples@gmail.com>
2014-02-18 20:32:19 +1100

    added list and delete functions

--- lib/epistle.py
+++ lib/epistle.py
@@ -6,11 +6,10 @@ import subprocess
 from datetime import datetime
 import ConfigParser
 
-TEMP_FILE = 'tmp'
-
 class Epistle():
 
   def __init__(self):
+    # file system stuff
     self.epistle_dir = os.path.join(os.path.expanduser('~'), ".epistle")
     self.epistle_cfg = os.path.join(self.epistle_dir, "epistle.ini")
     self.epistles_dir = os.path.join(self.epistle_dir, "epistles")
@@ -19,17 +18,20 @@ class Epistle():
     self.config.read(self.epistle_cfg)
 
   def get_vim(self):
-    # get vim 
     try:
       return self.config.get("Epistle","Vim")
     except:
       return os.environ.get('EDITOR','vim') # http://stackoverflow.com/questions/6309587/call-up-an-editor-vim-from-a-python-script
 
+  def list_epistles(self):
+    epistles = [ f for f in os.listdir(self.epistles_dir) if os.path.isfile(os.path.join(self.epistles_dir,f))]
+    for e in epistles:
+      print e
+
   def get_epistle(self,name):
     epistle_path = os.path.join(self.epistles_dir,name)
     if not os.path.isfile(epistle_path):
-      # make the file
-      os.system("touch %s" % epistle_path)
+      os.system("touch %s" % epistle_path)  # make the file
     return epistle_path
 
   def has_git_remote(self):
@@ -49,24 +51,61 @@ class Epistle():
   def git_push(self):
     os.system("cd %s && git push" % (self.epistles_dir))
 
-  def main(self, argv):
-    # update git repo (pull)
+  def git_rm(self, epistle):
+    deets = (self.epistles_dir, epistle, os.path.basename(epistle))
+    os.system("cd %s && git rm %s && git commit -m 'deleted %s'" % deets)
+    self.git_push()
+
+  def compose_epistle(self, name):
+    
     if self.has_git_remote():
       self.git_pull()
 
     vim = self.get_vim()
     print "Using vim from: %s" % vim
 
-    epistle = self.get_epistle(argv[1])
+    epistle = self.get_epistle(name)
     print "Got epistle file: %s" % epistle
 
+    # launch vim and wait for exit code
     subprocess.check_call([vim, epistle])
-    print "exited with success"
+
     # add and commit the changes. 
     self.git_commit(epistle)
     if self.has_git_remote():
       self.git_push()
 
+
+  def main(self, argv):
+    if len(argv) < 2:
+      self.print_help()
+      return
+    arg = argv[1]
+    if arg == "-h":
+      self.print_help()
+    elif arg == "-l":
+      self.list_epistles()
+    elif arg == "-d":
+      self.git_rm(argv[2])
+    else:
+      self.compose_epistle(arg)
+
+  def print_help(self):
+    print """
+    Epistle is a tool for taking notes in Vim and saving them using Git.
+
+    Usage:
+
+        $ epistle <something>     -> create or edit a note with this name
+
+        $ epistle -l              -> list available epistles
+
+        $ epistle -d <something>  -> delete an epistle
+
+        $ epistle -h              -> print this help
+
+    See more at http://github.com/msharp/epistle
+    """
 
 if __name__ == "__main__":
   e = Epistle()
